ad44cb0fa768726579bc879533f468cbc474630f
HuynhTrongTri <kenbistom99@gmail.com>
2021-04-21 10:11:12 +0700

    Second Settings

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,16 +3,80 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { MenuModule } from 'primeng/menu';
+import { CommonModule } from '@angular/common';
+// import { LoginModule } from './pages/login/login.module';
+// import { WebReqInterceptor } from './web-req.interceptor';
+import { AccordionModule } from 'primeng/accordion';
+import { DropdownModule } from 'primeng/dropdown';
+import { TooltipModule } from 'primeng/tooltip';
+import { TabViewModule } from 'primeng/tabview';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import { MainHeaderComponent } from './common/main-header/main-header.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
+import { SlidemenuComponent } from './common/slidemenu/slidemenu.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    SlidemenuComponent
+    
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CheckboxModule,
+    TableModule,
+    DialogModule,
+    ToastModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
+    HttpClientModule,
+    InputTextModule,
+    FormsModule,
+    TieredMenuModule,
+    SlideMenuModule,
+    BrowserAnimationsModule,
+    InputNumberModule,
+    MegaMenuModule,
+    MenuModule,
+    AccordionModule,
+    DropdownModule,
+    TooltipModule,
+    TabViewModule,
+    InputSwitchModule
   ],
-  providers: [],
+  providers: [MessageService, ConfirmationService,
+    // [
+    //   {
+    //     provide: HTTP_INTERCEPTORS,
+    //     useClass: WebReqInterceptor,
+    //     multi: true,
+    //   },
+    // ]
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Front-end/src/app/common/main-header/main-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.scss']
+})
+export class MainHeaderComponent implements OnInit {
+
+  items!: MenuItem[];
+  constructor(private router: Router) { }
+  userName: string = "Huynh Trong Tri";
+  ngOnInit(): void {
+    this.items = [{
+      items: [{
+        label: 'View Profile',
+        icon: 'pi pi-user-edit',
+        routerLink: '',
+      },
+      {
+        label: 'Log out',
+        icon: 'pi pi-sign-out',
+        routerLink: '/',
+      }
+      ]
+    },
+    ];
+  }
+  goToHome() {
+    this.router.navigateByUrl("/home");
+  }
+}
